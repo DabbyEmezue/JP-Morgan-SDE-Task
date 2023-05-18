76d5a2e029ad208e17c99ba8bdade59251168e62
Dabby Emezue <emezued@gmail.com>
2023-05-18 17:40:54 +0100

    Added Unit Tests

--- client3.py
+++ client3.py
@@ -30,8 +30,6 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
@@ -40,8 +38,6 @@ def getDataPoint(quote):
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
     if price_b == 0:
         return 
     ratio = price_a / price_b
@@ -50,11 +46,8 @@ def getRatio(price_a, price_b):
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
         prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
@@ -62,4 +55,3 @@ if __name__ == "__main__":
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
-
